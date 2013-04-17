c06eb0a03d748385ae8695676eed0153e84dad7c
Jenelle Walker <jen@gatech>
2013-04-16 22:27:21 -0400

    changed way attachment added to table

--- db/migrate/20130327173643_add_to_pass.rb
+++ db/migrate/20130327173643_add_to_pass.rb
@@ -2,12 +2,11 @@ class AddToPass < ActiveRecord::Migration
   def up
 	create_table :passes do |t|
 		t.string :passTypeIdentifier, :authenticationToken, :organizationName, :description, :logoText, :foregroundColor, :backgroundColor, :message, :key, :label, :value
-	end
-    add_attachment :passes, :logo2x
+	    t.attachment :logo2x
+    end
   end
 
   def down
-	remove_attachment :passes, :logo2x
     drop_table :passes
   end
 end
